27df999caefe368bdbf7de8bbdc58810ef4751ae
Robert Tejada <robert_tejada@ucne.edu.do>
2023-06-05 18:11:08 -0400

    Modelo Completado

---
+++ Models/Ingresos.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Ingresos
+{
+    public int IngresoId { get; set; }
+    public DateTime Fecha { get; set; }
+    
+    [Required(ErrorMessage = "El concepto es un campo obligatorio")]
+    public string? Concepto { get; set; }
+
+    [Required(ErrorMessage = "Es necesario especificar el monto")]
+    [Range(minimum: 1, maximum: 50000, ErrorMessage = "El monto debe estar entre 1 a 50000")]
+    public float Monto { get; set; }
+}
